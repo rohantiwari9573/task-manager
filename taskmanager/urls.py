7ecdfd582e2e09381c4d382495f897eb3d3fb161
Your Name <you@example.com>
2026-03-24 18:22:27 +0530

    final CRUD endpoints fix

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 def home(request):
     return JsonResponse({
@@ -17,6 +19,7 @@ def home(request):
         ]
     })
 
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Task Manager API",
@@ -24,14 +27,17 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    authentication_classes=[],
 )
+
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
+
     path('api/', include('tasks.urls')),
+
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
+
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
